743dcd72911a6d8b29ea425dc2129464f866461e
AnisaAyu219 <92657605+AnisaAyu219@users.noreply.github.com>
2023-10-03 11:49:53 +0700

    keduaa

--- app/Http/Controllers/BiodataController.php
+++ app/Http/Controllers/BiodataController.php
@@ -20,4 +20,18 @@ class BiodataController extends Controller
         $biodata->save();
         return response()->json($biodata, 200);
     }
+    public function inpu_biodata (Request $request){
+        $biodata=new Biodata();
+        $biodata->nim=$request->nim;
+        $biodata->pengalaman_kerja=$request->pengalaman_kerja;
+        $biodata->foto_profile=$request->file('foto_profile')->store('public/foto_profile');
+        $biodata->profile_singkat=$request->profile_singkat;
+        $biodata->skill=$request->skill;
+        $biodata->riwayat_pendidikan=$request->riwayat_pendidikan;
+
+
+        $biodata->save();
+        return response()->json($biodata, 200);
+    }
+
 }
